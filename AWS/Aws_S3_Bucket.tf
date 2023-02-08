3a0cbf642003eca3beceff26ceefb011dcce365d
ravicwp <109152895+ravicwp@users.noreply.github.com>
2023-02-08 13:48:09 +0530

    Update Aws_S3_Bucket.tf

--- AWS/Aws_S3_Bucket.tf
+++ AWS/Aws_S3_Bucket.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "examplebucket" {
   bucket = "examplebuckettftest"
 }
 
-resource "aws_s3_bucket" "mfadelete" {
+resource "aws_s3_bucket" "ravitesting1" {
   bucket = "examplebuckettftest"
   acl    = "private"
   versioning {
@@ -10,4 +10,5 @@ resource "aws_s3_bucket" "mfadelete" {
     mfa_delete = true
   }
 }
+
 
